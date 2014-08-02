1e13faf9b26f38f3566fabc5763e0dfe8649a70b
Sidney Harrell <sidney.harrell@gmail.com>
2014-08-01 21:55:36 -0400

    WP comment section

--- ee4_members.php
+++ ee4_members.php
@@ -1,8 +1,42 @@
-<?php
+<?php if ( ! defined('ABSPATH')) exit('No direct script access allowed');
+/*
+  Plugin Name: 	EE4 Members
+  Plugin URI: 	http://wordpress.org/plugins/event-espresso-free/
+  Description: 	Manage your events from your WordPress dashboard. Reduce your admin, reduce your costs, make your life easier! | <a href="admin.php?page=espresso_support&action=contact_support">Support</a>
+  Version: 	1.0.0.p
+  Author: 	Event Espresso
+  Author URI: 	http://eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
+  License: 	GPLv2
+  TextDomain: 	event_espresso
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+  Copyright 			(c) 2008-2014 Event Espresso  All Rights Reserved.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/**
+ * EE4 Members
+ *
+ * Extension for Event Espresso
+ *
+ * @package 	EE4-members
+ * @author 	Sidney Harrell
+ * @copyright	(c) 2014 Vivian-Harrell Web Development Studio, LLC  All Rights Reserved.
+ * @license 	{@link http://vhwebdev.com/support/terms-conditions/}   * see Plugin Licensing *
+ * @link 	{@link http://vhwebdev.com}
+ * @since 	4.0
+ *
  */
 

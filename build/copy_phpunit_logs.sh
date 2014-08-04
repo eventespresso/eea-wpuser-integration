48dc4e8e6444e49c89f90f35deedf7447b26619b
Sidney Harrell <sidney.harrell@gmail.com>
2014-08-04 17:08:11 -0400

    paths wrong in xml files

--- build/copy_phpunit_logs.sh
+++ build/copy_phpunit_logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SOURCEDIR="$1/wordpress-develop/src/wp-content/plugins/EE4/tests/build"
+SOURCEDIR="$1/wordpress-develop/src/wp-content/plugins/ee4-wpusers/tests/build"
 TARGETDIR="$1/build"
 cp -r $SOURCEDIR/logs $TARGETDIR
 cp -r $SOURCEDIR/coverage $TARGETDIR

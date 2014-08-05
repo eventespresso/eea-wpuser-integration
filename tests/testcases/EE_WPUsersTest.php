8a99969b87dc687f3b3dfc56cf338339d0642d02
Sidney Harrell <sidney.harrell@gmail.com>
2014-08-04 20:28:37 -0400

    try and grab a typical transaction

--- tests/testcases/EE_WPUsersTest.php
+++ tests/testcases/EE_WPUsersTest.php
@@ -13,6 +13,13 @@
  */
 class EE_WPUsersTest extends EE_UnitTestCase {
 	public function testFilterAnswerForWPUser() {
+		$transaction = $this->new_typical_transaction();
 		
+		ob_start();
+		var_dump($transaction);
+		$temp=  ob_get_clean();
+		file_put_contents('/tmp/log.txt',$temp, FILE_APPEND);
+		
+		assert('some answer' === 'some answer' );
 	}
 }

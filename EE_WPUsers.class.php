f2f14427e8b5501448f650f9038d792b72444928
Sidney Harrell <sidney.harrell@gmail.com>
2014-08-04 21:26:08 -0400

    should work now

--- EE_WPUsers.class.php
+++ EE_WPUsers.class.php
@@ -25,9 +25,33 @@ class EE_WPUsers extends EE_Addon {
 					)
 			)
 		);
+		
+		add_filter( 'FHEE__EEM_Answer__get_attendee_question_answer_value__answer_value', array( 'EE_WPUsers', 'filterAnswerForWPUser' ), 10, 3 );
+		
 	}
 	
 	public static function filterAnswerForWPUser($value, $registration, $question_id) {
+		if ( empty($value) ) {
+			$user_query = new WP_User_Query( array( 'meta_key' => 'EE_Attendee_ID', 'meta_value' => $registration->attendee_ID() ) );
+			if ( count($user_query) == 1 ) {
+				switch ($question_id) {
+					
+					case 1:
+						$value = $user_query[0]->get('first_name');
+						break;
+					
+					case 2:
+						$value = $user_query[0]->get('last_name');
+						break;
+					
+					case 3:
+						$value = $user_query[0]->get('user_email');
+						break;
+					
+					default:
+				}
+			}
+		}
 		return $value;
 	}
 }
--- tests/testcases/ee-addon-wpusers-tests.php
+++ tests/testcases/ee-addon-wpusers-tests.php
@@ -17,5 +17,6 @@ class ee_addon_wpusers_tests extends EE_UnitTestCase {
 	function test_load_ee_core_wpusers() {
 		$this->assertEquals( has_action('AHEE__EE_System__load_espresso_addons', 'load_ee_core_wpusers'), 10 );
 		$this->assertTrue( class_exists( 'EE_WPUsers' ) );
+		$this->assertEquals(has_filter('AHEE__EE_System__load_espresso_addons', 'filterAnswerForWPUser'), 10 );
 	}
 }

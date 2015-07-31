d9d9f3e2386ca59cfee39a2200d7bb72ef18a551
Michael Nelson <michael@eventespresso.com>
2015-07-31 12:20:10 -0700

    [touch:8596]{t:90}override a bunch of parent methods in order to account for partially migrated data from 2.0.1.p. The main difference is that we are NOT using MYSQL offsets to select the next rows to migrate, but are instead running until we have used up all the old meta rows

--- core/data_migration_scripts/2_0_0_stages/EE_DMS_2_0_0_user_option.dmsstage.php
+++ core/data_migration_scripts/2_0_0_stages/EE_DMS_2_0_0_user_option.dmsstage.php
@@ -86,4 +86,52 @@ class EE_DMS_2_0_0_user_option extends EE_Data_Migration_Script_Stage_Table {
 		delete_user_meta( $userid, 'EE_Attendee_ID' );
 	}
 
+	/**
+	 * Overrides parent because we only want to stop when we are certain there
+	 * are no more original records left, in order to solve #8596
+	 * @param int $num_items
+	 * @return int number of items ACTUALLY migrated
+	 */
+	function _migration_step($num_items=50){
+		$rows = $this->_get_rows( $num_items );
+		$items_actually_migrated = 0;
+		foreach($rows as $old_row){
+			$this->_migrate_old_row($old_row);
+			$items_actually_migrated++;
+		}
+		if( ! $this->_records_remaining() ){
+			$this->set_completed();
+		}
+		return $items_actually_migrated;
+	}
+
+	/**
+	 * We need an accurate count of how many more records need migration, in order to solve #8596
+	 * @global wpdb $wpdb
+	 * @return int
+	 */
+	protected function _records_remaining() {
+		global $wpdb;
+		$query =  "SELECT COUNT(*) FROM {$this->_old_table} WHERE meta_key = 'EE_Attendee_ID'";
+		$count = $wpdb->get_var( $query );
+		return intval( $count );
+	}
+
+	/**
+	 * Overrides parent because we want ot make sure we fetch ALL the records
+	 * (because earlier code modified the original queryset meaning that once
+	 * we got halfway through migrating this stage's data, we had also removed
+	 * half the data we were working on, but were using offsets and so were
+	 * trying to grab data from the 2nd half of the queryset whcih was now half the size,
+	 * so we were getting nothing and were looping)
+	 * @global wpdb $wpdb
+	 * @param int $limit
+	 * @return array of arrays like $wpdb->get_results($sql, ARRAY_A)
+	 */
+	protected function _get_rows( $limit ){
+		global $wpdb;
+		$query = "SELECT * FROM {$this->_old_table} WHERE meta_key = 'EE_Attendee_ID' LIMIT $limit";
+		return $wpdb->get_results($query,ARRAY_A);
+	}
+
 } //end of EE_DMS_2_0_0_user_option.dmsstage.php

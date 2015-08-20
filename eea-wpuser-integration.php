fe8b3cba4bc43d6b43dd1e894dbc9bd1e045d446
EE DevBox Server <admin@eventespresso.com>
2015-08-20 16:11:13 -0400

    Bumping version to 2.0.4.rc.003

--- eea-wpuser-integration.php
+++ eea-wpuser-integration.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) )
   Plugin Name: 	Event Espresso - WP Users (EE4.6+)
   Plugin URI: 	http://www.eventespresso.com
   Description: 	This adds the WP users integration.
-  Version: 	2.0.4.rc.002
+  Version: 	2.0.4.rc.003
   Author: 	Event Espresso
   Author URI: 	http://www.eventespresso.com
   License: 	GPLv2
@@ -35,7 +35,7 @@ if ( ! defined( 'ABSPATH' ) )
  * @package 	EE WPUsers
  *
  */
-define( 'EE_WPUSERS_VERSION', '2.0.4.rc.002' );
+define( 'EE_WPUSERS_VERSION', '2.0.4.rc.003' );
 define( 'EE_WPUSERS_PLUGIN_FILE', __FILE__ );
 
 

58c037aa001b48e35359cbf998d94fcc82117a86
Darren Ethier <darren@roughsmootheng.in>
2017-04-20 15:48:34 -0400

    update bootstrap and config for tests on eea-wpuser-integration [touch: 10623]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,34 +1,21 @@
 <?php
-
 /**
- * Bootstrap for EE_WPUsers Unit Tests
- *
- * @since 		1.0.0
- * @package 	EE WPUsers
- * @subpackage 	Tests
+ * Bootstrap for eea-wpuser-integration tests
  */
-require( dirname( __FILE__ ) . '/includes/define-constants.php' );
-if ( ! is_readable( WP_TESTS_DIR . '/includes/functions.php' ) ) {
-	die( "The WordPress PHPUnit test suite could not be found.\n" );
-}
 
-require_once WP_TESTS_DIR . '/includes/functions.php';
+use EETests\bootstrap\AddonLoader;
 
+$core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+require $core_tests_dir . 'includes/CoreLoader.php';
+require $core_tests_dir . 'includes/AddonLoader.php';
 
-
-
-
-function _install_and_load_core_and_ee_promos() {
-	require EE_TESTS_DIR . 'includes/loader.php';
-	require EE_WPUSERS_TESTS_DIR . 'includes/loader.php';
-
-}
+define('EE_WPUSERS_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
+define('EE_WPUSERS_TESTS_DIR', EE_WPUSERS_PLUGIN_DIR . 'tests/');
 
 
-
-tests_add_filter( 'muplugins_loaded', '_install_and_load_core_and_ee_promos' );
-
-require WP_TESTS_DIR . '/includes/bootstrap.php';
-
-//Load the EE_specific testing tools
-require EE_TESTS_DIR . 'includes/EE_UnitTestCase.class.php';
+$addon_loader = new AddonLoader(
+    EE_WPUSERS_TESTS_DIR,
+    EE_WPUSERS_PLUGIN_DIR,
+    'eea-people-addon.php'
+);
+$addon_loader->init();

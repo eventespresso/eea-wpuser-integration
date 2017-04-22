3c46c298e9a2094236ad6e19d8905fa63d69f3e3
Darren Ethier <darren@roughsmootheng.in>
2017-04-22 15:20:10 -0400

    update tests bootstrap and config for wp user integration add-on [touch :10623]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,10 @@
 use EETests\bootstrap\AddonLoader;
 
 $core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+//if still don't have $core_tests_dir, then let's check tmp folder.
+if (! is_dir($core_tests_dir)) {
+    $core_tests_dir = '/tmp/event-espresso-core/tests/';
+}
 require $core_tests_dir . 'includes/CoreLoader.php';
 require $core_tests_dir . 'includes/AddonLoader.php';
 

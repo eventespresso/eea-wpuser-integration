681d22ae56af75d8d3b96ae26128bcd172c066e7
Darren Ethier <darren@roughsmootheng.in>
2017-08-09 15:10:21 -0400

    formatting and code cleanup [touch: 9372]

--- templates/content-espresso_my_events-event_section_tickets.template.php
+++ templates/content-espresso_my_events-event_section_tickets.template.php
@@ -20,8 +20,8 @@ $ticket = $registration->ticket();
         $actions = array();
         //only show the edit registration link IF the registration has question groups.
         $actions['edit_registration'] = $registration->count_question_groups()
-            ? '<a aria-label="' . __('Link to edit registration',
-                'event_espresso') . '" href="' . $registration->edit_attendee_information_url() . '">'
+            ? '<a aria-label="' . __('Link to edit registration', 'event_espresso')
+              . '" href="' . $registration->edit_attendee_information_url() . '">'
               . '<span class="ee-icon ee-icon-user-edit ee-icon-size-16"></span></a>'
             : '';
         //resend confirmation email.
@@ -31,38 +31,40 @@ $ticket = $registration->ticket();
         );
         if ($registration->is_primary_registrant() ||
             (! $registration->is_primary_registrant()
-             && $registration->status_ID() == EEM_Registration::status_id_approved)) {
-
-            $actions['resend_registration'] = '<a aria-label="' . __('Link to resend registration message',
-                    'event_espresso') . '" href="' . $resend_registration_link . '">'
-                                              . '<span class="dashicons dashicons-email-alt"></span></a>';
+             && $registration->status_ID() == EEM_Registration::status_id_approved)
+        ) {
+            $actions['resend_registration'] = '<a aria-label="'
+                . __('Link to resend registration message', 'event_espresso')
+                . '" href="' . $resend_registration_link . '">'
+                . '<span class="dashicons dashicons-email-alt"></span></a>';
         }
 
         //make payment?
         if ($registration->is_primary_registrant()
             && $registration->transaction() instanceof EE_Transaction
             && $registration->transaction()->remaining()) {
-            $actions['make_payment'] = '<a aria-label="' . __('Link to make payment',
-                    'event_espresso') . '" href="' . $registration->payment_overview_url() . '">'
+            $actions['make_payment'] = '<a aria-label="' . __('Link to make payment', 'event_espresso')
+                                       . '" href="' . $registration->payment_overview_url() . '">'
                                        . '<span class="dashicons dashicons-cart"></span></a>';
         }
 
         //receipt link?
         if ($registration->is_primary_registrant() && $registration->receipt_url()) {
-            $actions['receipt'] = '<a aria-label="' . __('Link to view receipt',
-                    'event_espresso') . '" href="' . $registration->receipt_url() . '">'
+            $actions['receipt'] = '<a aria-label="' . __('Link to view receipt', 'event_espresso')
+                                  . '" href="' . $registration->receipt_url() . '">'
                                   . '<span class="dashicons dashicons-media-default ee-icon-size-18"></span></a>';
         }
 
         //invoice link?
         if ($registration->is_primary_registrant() && $registration->invoice_url()) {
-            $actions['invoice'] = '<a aria-label="' . __('Link to view invoice',
-                    'event_espresso') . '" href="' . $registration->invoice_url() . '">'
+            $actions['invoice'] = '<a aria-label="' . __('Link to view invoice', 'event_espresso')
+                                  . '" href="' . $registration->invoice_url() . '">'
                                   . '<span class="dashicons dashicons-media-spreadsheet ee-icon-size-18"></span></a>';
         }
 
         //filter actions
-        $actions = apply_filters('FHEE__EES_Espresso_My_Events__actions',
+        $actions = apply_filters(
+            'FHEE__EES_Espresso_My_Events__actions',
             $actions,
             $registration
         );

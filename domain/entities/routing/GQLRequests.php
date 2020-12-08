2453d220fb2d612bf0cb836e17a188a89932e11c
Brent Christensen <brent@eventespresso.com>
2020-12-08 12:08:40 -0800

    WP User GQLRequests

---
+++ domain/entities/routing/GQLRequests.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EventEspresso\WpUser\domain\entities\routing;
+
+use EventEspresso\core\domain\entities\routing\handlers\shared\GQLRequests as CoreGQLRequests;
+use EventEspresso\WpUser\domain\services\graphql\RegisterSchema;
+
+/**
+ * Class GQLRequests
+ *
+ * @author  Brent Christensen
+ * @package EventEspresso\WpUser\domain\entities\routing
+ * @since   $VID:$
+ */
+class GQLRequests extends CoreGQLRequests
+{
+
+    protected function registerDependencies()
+    {
+    }
+
+
+    /**
+     * implements logic required to run during request
+     *
+     * @return bool
+     */
+    protected function requestHandler()
+    {
+        /** @var RegisterSchema $schema */
+        $schema = $this->loader->getShared(RegisterSchema::class);
+        $schema->addHooks();
+        return true;
+    }
+}

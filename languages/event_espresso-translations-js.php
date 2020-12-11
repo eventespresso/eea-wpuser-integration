2a255f7878df790927321deb09430f6609db0272
eebot <team@eventespresso.com>
2020-12-11 12:30:09 +0000

    Deployed from eventespresso/barista@20504e737a8e5913a91916da76ca68c1cc6fedd9

---
+++ languages/event_espresso-translations-js.php
@@ -0,0 +1,22 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: domains/wpUser/src/hooks/useCapabilityOptions.ts:39
+	__( 'Custom', 'event_espresso' ),
+
+	// Reference: domains/wpUser/src/hooks/useCapabilityOptions.ts:43
+	__( 'Custom capability', 'event_espresso' ),
+
+	// Reference: domains/wpUser/src/hooks/useTicketFormSections.ts:24
+	__( 'WP Users', 'event_espresso' ),
+
+	// Reference: domains/wpUser/src/hooks/useTicketFormSections.ts:28
+	__( 'Ticket Capability Requirement', 'event_espresso' ),
+
+	// Reference: domains/wpUser/src/hooks/useTicketFormSections.ts:31
+	__( 'It enables you to set restrictions on who can purchase the ticket option. This is an excellent way to create "Member Only" type discounts to people visiting your site.', 'event_espresso' ),
+
+	// Reference: domains/wpUser/src/hooks/useTicketFormSections.ts:37
+	__( 'Custom Capability', 'event_espresso' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
